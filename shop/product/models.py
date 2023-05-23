0c2cc04ad2f225af1ab760ff3dd4c971d32d8fa5
nguyen dang <nguyendang1547@gmail.com>
2023-05-23 21:24:19 +0700

    Create product model

--- shop/product/models.py
+++ shop/product/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    title = models.CharField(default="")
+    slug = models.CharField(max_length=100, default="")
+    desc = models.TextField(default="")
+    active = models.BooleanField(default=True)
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=255, default="")
+    desc = models.TextField(default="")
+    price = models.IntegerField(default=0)
+    active = models.BooleanField(default=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    
+
+class Variation(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255, default="")
+    price = models.IntegerField(default=0)
+    sale_price = models.IntegerField(default=0)
+    active = models.BooleanField(default=True)
+    inventory = models.IntegerField(default=0)
+    
+
