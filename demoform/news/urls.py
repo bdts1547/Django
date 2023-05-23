0ec6ff876c2d19dd0fee570a820ed97d934f9d8a
nguyen dang <nguyendang1547@gmail.com>
2023-05-23 21:08:01 +0700

    Admin custom

--- demoform/news/urls.py
+++ demoform/news/urls.py
@@ -6,7 +6,6 @@ from . import views
 app_name = 'news'
 urlpatterns = [
     path('', views.IndexClass.as_view(), name='index'),
-    # path('add/', views.AddClass.as_view(), name='add'),
     path('save/', views.SaveNewsClass.as_view(), name='save'),
     path('email/', views.email_view, name='email'),
     path('sent/', views.email_sent, name='sent'),
