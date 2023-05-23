73604f2486a8c9b567150a6a4b6a84acc023dc01
nguyen dang <nguyendang1547@gmail.com>
2023-05-23 10:39:48 +0700

    Widget with model form

--- demoform/news/forms.py
+++ demoform/news/forms.py
@@ -6,12 +6,17 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'content', 'time_public', )
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'title'}),
+            'content': forms.Textarea(attrs={'class': 'content'})
+
+        }
 
 
 class SendEmail(forms.Form):
-    title = forms.CharField(max_length=100)
+    title = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'email_title'}))
     email = forms.EmailField()
-    content = forms.CharField(widget=forms.Textarea)
-    cc = forms.BooleanField(require=False)
+    content = forms.CharField(widget=forms.Textarea(attrs={'class': 'email_content', 'id': 'email_id'}))
+    cc = forms.BooleanField(required=False)
 
     
