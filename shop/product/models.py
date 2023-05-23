07dea4a1bca1fbee635c3ff3f6e8163c962a08e2
nguyen dang <nguyendang1547@gmail.com>
2023-05-23 21:42:00 +0700

    Makemigrations & migrate

--- shop/product/models.py
+++ shop/product/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Category(models.Model):
-    title = models.CharField(default="")
+    title = models.CharField(default="", max_length=100)
     slug = models.CharField(max_length=100, default="")
     desc = models.TextField(default="")
     active = models.BooleanField(default=True)
--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
+AUTH_USER_MODEL = "user.CustomerUser"
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
