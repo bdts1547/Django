caebfb9be42ae954a3e160365de88a8d2733c12b
nguyen dang <nguyendang1547@gmail.com>
2023-05-24 09:50:04 +0700

    Add templates

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'cart',
     'order',
     'user',
+    'core',
 
 ]
 
@@ -59,7 +60,9 @@ ROOT_URLCONF = 'shop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
+
+
