2e2d495e62b64e110f88bb294422cce1785181d8
nguyen dang <nguyendang1547@gmail.com>
2023-06-14 20:33:33 +0700

    comple geoLocation very simple

--- freeCodeCamp/geoLocation/location/views.py
+++ freeCodeCamp/geoLocation/location/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views import View
+import requests
+import json
+
 
 # Create your views here.
+class Home(View):
+    def get(self, request):
+        ip_res = requests.get("https://api.ipify.org?format=json")
+        ip = json.loads(ip_res.text)
+        res = requests.get(f"http://ip-api.com/json/{ip['ip']}")
+        location_data = json.loads(res.text)
+        
+
+        return render(request, 'index.html', {'data': location_data})
